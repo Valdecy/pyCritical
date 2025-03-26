2dfc31b26a8736adb7a5d17c3bcf699be81f374f
Valdecy Pereira <valdecypereira@yahoo.com.br>
2025-03-26 20:35:15 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='pyCritical',
-    version='1.5.2',
+    version='1.5.7',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
